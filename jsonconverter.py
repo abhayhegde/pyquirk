777cf5c80a005b6400b5aa81586ca8bd28268c01
abhayhegde <abhayhegde.skp@gmail.com>
2020-08-17 12:24:19 +0530

    classical wire for measured gates

--- jsonconverter.py
+++ jsonconverter.py
@@ -4,16 +4,16 @@ A simple python program to convert quantum circuits to TeX code.
 Acknowledgements: https://algassert.com/quirk
 """
 
+import sys
 import json
 
-file = open("circuit.txt", "r")
+file = open(str(sys.argv[1]), "r")
 data = json.load(file)
 nrow = max([len(i) for i in data['cols']])
 
-""" targ_pos = 0
-if "•" in data['cols']:
-    targ_pos = data['cols'].index("•") - data['cols'].index("X")
-print(targ_pos) """
+# if "•" in data['cols']:
+#     targ_pos = data['cols'].index("•") - data['cols'].index("X")
+# print(targ_pos)
 
 if data.get('init') == None:
     data['init'] = [0] * nrow
@@ -24,32 +24,39 @@ for i in range(len(data['cols'])):
     data['cols'][i] += [1] * (nrow - len(data['cols'][i]))
 data['rows'] = list(map(list, zip(*data.pop('cols'))))
 
-quantikz_env = ["\\begin{quantikz}", "\end{quantikz}"]
 initial_state = []
-
 for state in range(len(data['init'])):
     initial_state.append(
         ''.join(["\lstick{\ket{", str(data['init'][state]), "}}"]))
 
-subs = dict(zip([1, "X", "Y", "Z", "H", "•", "Measure"],
-                ["\qw", "\gate{X}", "\gate{Y}", "\gate{Z}", "\gate{H}", "\ctrl{1}", "\meter{}"]))
+subs = dict(zip([1, "X", "Y", "Z", "H", "U", "•", "Measure", "X1"],
+                ["\qw", "\gate{X}", "\gate{Y}", "\gate{Z}", "\gate{H}", "\gate{U}", "\ctrl{1}", "\meter{}", "\targ{}"]))
 
 comp = []
 for i in range(nrow):
-    a = [subs[ele] for ele in data['rows'][i]]
+    a = [''.join(["\gate{", ele, "}"]) if ele not in subs else subs[ele] for ele in data['rows'][i]]
     comp.append(a)
 
-code = open("code.tex", "w")
-preamble = "\\documentclass{article}\n\\usepackage{tikz}\n\\usetikzlibrary{quantikz}\n\\begin{document}\n"
-end = "\\end{document}"
-code.write(preamble)
-code.write(''.join([quantikz_env[0], "\n"]))
+main = []
 for i in range(nrow):
     if i == nrow - 1:
-        code.write(''.join([initial_state[i], '&', ' & '.join(comp[i]), '\n']))
+        main.append(''.join([initial_state[i], '&', ' & '.join(comp[i]), '& \qw', '\n']))
     else:
-        code.write(
-            ''.join([initial_state[i], '&', ' & '.join(comp[i]), '\\\\', '\n']))
-code.write(''.join([quantikz_env[1], "\n"]))
+        main.append(
+            ''.join([initial_state[i], '&', ' & '.join(comp[i]), '& \qw ', '\\\\', '\n']))
+
+met_index = [i.find('\\meter{}') for i in main]
+if max(met_index) > -1:
+    for i in range(len(met_index)):
+        if met_index[i] > -1:
+            old_str = main[i][met_index[i]+len('\\meter{} &'):]
+            new_str = old_str.replace("\qw", "\cw")
+            main[i] = main[i][:len('\\meter{} &')+met_index[i]] + new_str
+
+code = open(sys.argv[2], "w")
+preamble = "\\documentclass{article}\n\\usepackage{tikz}\n\\usetikzlibrary{quantikz}\n\\begin{document}\n\\begin{quantikz}\n"
+end = "\\end{quantikz}\n\\end{document}"
+code.write(preamble)
+code.write(''.join(main))
 code.write(end)
 code.close()
