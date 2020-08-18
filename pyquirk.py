cacf222484101a11e1d29b8b837703d633ff1f8b
abhayhegde <abhayhegde.skp@gmail.com>
2020-08-18 13:27:10 +0530

    multiple control works

--- pyquirk.py
+++ pyquirk.py
@@ -10,34 +10,40 @@ import json
 file = open(str(sys.argv[1]), "r")
 data = json.load(file)
 nrow = max([len(i) for i in data['cols']])
+special = ["•", "◦"]
+subcol = []    
+vqw_ind = []
+comp = []
+main = []
 
 for col in data['cols']:
-    if "•" in col:
-        nt_gates = [gate for gate in col if gate != 1]
-        ctrl_index = col.index("•")
-        targ_index = col.index(next(gate for gate in nt_gates if gate not in ["•"]))
-        diff = targ_index - ctrl_index
-        col[ctrl_index] = ''.join(["\\ctrl{", str(diff), "}"])
-        indices = [i for i, x in enumerate(col) if x == "•"]
-        for i in indices: col[i] = "\\ctrl{}"
+    subcols = [''] * len(col)
+    if any(s in col for s in special):
+        inds1 = [i for i, x in enumerate(col) if x == "•"] 
+        inds2 = [i for i, x in enumerate(col) if x == "◦"] 
+        if len(inds1) == 0: ctrl_ind = inds2[0]
+        elif len(inds2) == 0: ctrl_ind = inds1[0]
+        else: ctrl_ind = min([inds1[0], inds2[0]]) 
+        nt_gates = [gate for gate in col if gate not in special and gate != 1]
+        targ_ind = col.index(nt_gates[0])
+        diff = targ_ind - ctrl_ind
+        for i in inds1: col[i] = "\\ctrl{}"
+        for i in inds2: col[i] = "\\octrl{}"
+        if col[ctrl_ind] == "\\octrl{}":
+           col[ctrl_ind] = ''.join(["\\octrl{", str(diff), "}"])
+        else:
+           col[ctrl_ind] = ''.join(["\\ctrl{", str(diff), "}"])
+        vgates = []
+        lst = ['ctrl', "1"]
+        for gate in col:
+            if any([x in str(gate) for x in lst]): pass
+            else: vgates.append(gate)
+        cols = [str(x) for x in col]
         if "X" in nt_gates: col[col.index("X")] = "\\targ{}"
-        # vgates = []
-        # lst = ['ctrl', "1"]
-        # for gate in col:
-        #     if any([x in str(gate) for x in lst]): pass
-        #     else: vgates.append(gate)
-        # for i in range(1, len(vgates)):
-        #     vdiff = col.index(vgates[i]) - col.index(vgates[i-1])
-        #     col[i+1] += ''.join([" \\vqw {", str(vdiff), "}"])
-    
-    if "◦" in col:
-        nt_gates = [gate for gate in col if gate != 1]
-        octrl_index = col.index("◦")
-        otarg_index = col.index(next(gate for gate in nt_gates if gate != "◦"))
-        diff = otarg_index - octrl_index
-        col[octrl_index] = ''.join(["\\octrl{", str(diff), "}"])
-        if "X" in nt_gates: col[col.index("X")] = "\\targ{}"
-
+        for i in range(len(vgates) - 1):
+            vdiff = cols.index(vgates[i+1]) - cols.index(vgates[i])
+            subcols[i+1] = ''.join([" \\vqw {", str(vdiff), "}"])
+    subcol.append(subcols)
     if "Swap" in col:
         pos = []
         for i in range(len(col)):
@@ -63,19 +69,28 @@ for state in range(len(data['init'])):
 subs = dict(zip([1, "X", "Y", "Z", "H", "U", "Measure", "Z^½", "Z^¼", "Z^-½","Z^-¼", "Y^½", "Y^¼", "Y^-½","X^-¼", "X^½", "X^¼", "X^-½","X^-¼", "…"],
                 ["\qw", "\gate{X}", "\gate{Y}", "\gate{Z}", "\gate{H}", "\gate{U}", "\meter{}", "\\gate{S}", "\\gate{T}","\\gate{S^{-1}}", "\\gate{T^{-1}}", "\\gate{Y^{\\frac{1}{2}}}", "\\gate{Y^{\\frac{1}{4}}}","\\gate{Y^{-\\frac{1}{2}}}", "\\gate{Y^{-\\frac{1}{4}}}", "\\gate{X^{\\frac{1}{2}}}", "\\gate{X^{\\frac{1}{4}}}","\\gate{X^{-\\frac{1}{2}}}", "\\gate{X^{-\\frac{1}{4}}}", "\qw"]))
 
-comp = []
-main = []
+for i in range(len(subcol)):
+    for j in range(len(subcol[i])):
+        if any([x in subcol[i][j] for x in "vqw"]):
+            vqw_ind.append([j, i])
+
 ## substitutions
 for i in range(nrow):
     # row = [''.join(["\gate{", ele, "}"]) if ele not in subs else subs[ele] for ele in data['rows'][i]]
+    # leave = ['ctrl', 'targ', 'gate']
     row = [subs[ele] if ele in subs else ele if "ctrl" in str(ele) else ele if "targ" in str(ele) else ele if "swap" in str(ele) else ''.join(["\gate{", ele, "}"]) for ele in data['rows'][i]]
     comp.append(row)
+
+for i, j in vqw_ind:
+    comp[i][j] += subcol[j][i]
+
+for i in range(nrow):
     if i == nrow - 1:
         main.append(''.join([initial_state[i], '&', ' & '.join(comp[i]), '& \qw', '\n']))
     else:
         main.append(
             ''.join([initial_state[i], '&', ' & '.join(comp[i]), '& \qw ', '\\\\', '\n']))
-            
+
 ## to replace with classical wires if meter is present
 met_index = [i.find('\\meter{}') for i in main]
 if max(met_index) > -1:
